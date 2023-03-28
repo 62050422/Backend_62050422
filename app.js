3d441d73fc80957ef394557fd85000005e36c9e2
unknown <62050422@go.buu.ac.th>
2023-03-28 11:34:52 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const mongoose = require("mongoose");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const productRouter = require("./routes/products");
+const accountRouter = require("./routes/accounts");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://PanuddaK:Pako422@cluster0.5ueyivs.mongodb.net/test"
+);
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/products", productRouter);
+app.use("/accounts", accountRouter);
+
+module.exports = app;
